316067c7a6e565dad53b5c700a1c148f94600621
Willian Menezes <willian_menezes_santos@hotmail.com>
2021-12-11 17:09:55 -0300

    Ajustado exemplo para entender injecao de dependencia. Removendo o HostBuilder e configurando apenas um service collection

---
+++ InjecaoDeDependencia.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace DependencyInjection
+{
+    public class InjecaoDeDependencia
+    {
+        public void ExecutarMetodosDeTestes(string[] args)
+        {
+            var servicerCollection = new ServiceCollection();
+
+            servicerCollection
+                .AddTransient<IOperacaoTransient, Operacao>()
+                .AddScoped<IOperacaoScoped, Operacao>()
+                .AddSingleton<IOperacaoSingleton, Operacao>();
+
+            var serviceProvider = servicerCollection.BuildServiceProvider();
+
+            VerificarTipoInjecaoDependencia(serviceProvider, "01 - ");
+            VerificarTipoInjecaoDependencia(serviceProvider, "02 - ");
+        }
+
+        private void VerificarTipoInjecaoDependencia(IServiceProvider services, string scope)
+        {
+            using IServiceScope serviceScope = services.CreateScope();
+
+            var transient = serviceScope.ServiceProvider.GetRequiredService<IOperacaoTransient>();
+            var scoped = serviceScope.ServiceProvider.GetRequiredService<IOperacaoScoped>();
+            var singleton = serviceScope.ServiceProvider.GetRequiredService<IOperacaoSingleton>();
+
+            var transient2 = serviceScope.ServiceProvider.GetRequiredService<IOperacaoTransient>();
+            var scoped2 = serviceScope.ServiceProvider.GetRequiredService<IOperacaoScoped>();
+            var singleton2 = serviceScope.ServiceProvider.GetRequiredService<IOperacaoSingleton>();
+
+            Console.WriteLine($"{scope}transient - { transient.Id}");
+            Console.WriteLine($"{scope}scoped - { scoped.Id}");
+            Console.WriteLine($"{scope}singleton - { singleton.Id}");
+
+            Console.WriteLine("===========================================");
+
+            Console.WriteLine($"{scope}transient - { transient2.Id}");
+            Console.WriteLine($"{scope}scoped - { scoped2.Id}");
+            Console.WriteLine($"{scope}singleton - { singleton2.Id}");
+
+            Console.WriteLine("===========================================");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,52 +9,7 @@ namespace DependencyInjection
     {
         static void Main(string[] args)
         {
-            using IHost host = CreateHostBuilder(args).Build();
-
-            VerificarTipoInjecaoDependencia(host.Services, "01 - ");
-            VerificarTipoInjecaoDependencia(host.Services, "02 - ");
-
-            host.Run();
-
+            new InjecaoDeDependencia().ExecutarMetodosDeTestes(args);
         }
-
-        static IHostBuilder CreateHostBuilder(string[] args) =>
-           Host.CreateDefaultBuilder(args)
-               .ConfigureServices((_, services) =>
-               {
-                   services.AddTransient<IOperacaoTransient, Operacao>()
-                         .AddScoped<IOperacaoScoped, Operacao>()
-                         .AddSingleton<IOperacaoSingleton, Operacao>();
-
-                   // configurando injeção com classes genéricas
-                   services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-               });
-
-        static void VerificarTipoInjecaoDependencia(IServiceProvider services, string scope)
-        {
-            using IServiceScope serviceScope = services.CreateScope();
-            IServiceProvider provider = serviceScope.ServiceProvider;
-
-            var transient = provider.GetRequiredService<IOperacaoTransient>();
-            var scoped = provider.GetRequiredService<IOperacaoScoped>();
-            var singleton = provider.GetRequiredService<IOperacaoSingleton>();
-
-            var transient2 = provider.GetRequiredService<IOperacaoTransient>();
-            var scoped2 = provider.GetRequiredService<IOperacaoScoped>();
-            var singleton2 = provider.GetRequiredService<IOperacaoSingleton>();
-
-            Console.WriteLine($"{scope}transient - { transient.Id}");
-            Console.WriteLine($"{scope}scoped - { scoped.Id}");
-            Console.WriteLine($"{scope}singleton - { singleton.Id}");
-
-            Console.WriteLine("===========================================");
-
-            Console.WriteLine($"{scope}transient - { transient2.Id}");
-            Console.WriteLine($"{scope}scoped - { scoped2.Id}");
-            Console.WriteLine($"{scope}singleton - { singleton2.Id}");
-
-            Console.WriteLine("===========================================");
-        }
-
     }
 }
